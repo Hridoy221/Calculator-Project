0a3d841ed10b3b6ea29f6025e45dcf093dd66b3c
Hridoy221 <107703499+Hridoy221@users.noreply.github.com>
2022-07-24 16:02:23 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var run = document.getElementById("run");
+
+
+run.addEventListener("click", ()=> {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var output = document.getElementById("result");
+
+    if(Number.isInteger(num1) && Number.isInteger(num2)){
+        var op = document.getElementById("op").value;
+
+        switch(op){
+            case "add":
+                var add = num1 + num2;
+                output.innerHTML = "The result of " + num1 + "+" + num2 + " is " + add;
+                break;
+    
+            case "sub":
+                var sub = num1 - num2;
+                output.innerHTML = "The result of " + num1 + "-" + num2 + " is " + sub;
+                break;
+    
+            case "mul":
+                var mul = num1 * num2;
+                output.innerHTML = "The result of " + num1 + "*" + num2 + " is " + mul;
+                break;
+    
+            case "div":
+                var div = num1 / num2;
+                output.innerHTML = "The result of " + num1 + "/" + num2 + " is " + div;
+                break;
+    
+            default:
+                output.innerHTML = "Invalide!!";
+                break;
+        }
+    }else{
+        output.innerHTML = "Input fields are empty";
+    }
+});
